1881608715a2f570ba199a085a3aae2ea15186b4
Royal <royalhuseyn902@gmail.com>
2022-01-13 16:18:56 +0900

    Created a new array called final_array on which there can be done some operations

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
 require "pry-byebug"
 
 def bubble_sort(array)
-  array.each do |element|
-    previous_element_set = false
-    if previous_element_set
-      binding.pry
-      if previous_element > element
-        array[array.index(element)] = previous_element
-        array[array.index(previous_element)] = element
-        previous_element = element
-        previous_element_set = true
-      end
+  final_array = array
+  array.each_with_index do |element, index|
+    if element == final_array[0]
+      next
+    end
+    
+    if final_array[index - 1] > element
+      final_array[index] = final_array[index - 1]
+      final_array[index - 1] = element
     end
   end
-  array
+  final_array
 end
 
 p bubble_sort([4, 3, 2, 1])
