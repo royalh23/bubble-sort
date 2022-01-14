ac923649033858b648440b709b47f4409e1e5c82
Royal <royalhuseyn902@gmail.com>
2022-01-14 20:03:57 +0900

    Added comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,18 @@
 require "pry-byebug"
 
 def bubble_sort(array)
+  # An array to hold the copy of the original array
   final_array = array
+
+  # Loop over the array
   array.each_with_index do |element, index|
+    # Skip to the next iteration if the element is the first element
     if element == final_array[0]
       next
     end
     
+    # Compare the current and the last element and change their indices if the
+    # last one is greater than the current one
     if final_array[index - 1] > element
       final_array[index] = final_array[index - 1]
       final_array[index - 1] = element
